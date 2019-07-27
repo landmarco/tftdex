d5a35db91aa90cc3100d6d86a3b9339f49e4c236
landmarco <marc.loeffke@gmail.com>
2019-07-27 19:37:14 +1200

    deletes initialization cruft from muk_minimizer

--- muk_minimizer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""muk_minimizer - A summarizer primarily for legal opinions and documents"""
-
-__version__ = '0.1.0'
-__author__ = 'Marc Loeffke <marc.loeffke@gmail.com>'
-__all__ = []
-
-from muk_minimizer import muk_minimizer
